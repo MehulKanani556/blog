d0da911d337c8cfc769e72f5d0aa797750bdd29b
Mehul Kanani <mkanani556@gmail.com>
2026-06-25 15:08:15 +0530

    Refactor GoogleAd component to use useRef for ad element and improve ad loading logic

--- front/src/pages/GoogleAd.js
+++ front/src/pages/GoogleAd.js
@@ -1,19 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export default function GoogleAd({adClient = "ca-pub-1234567890123456",adSlot="1234567890"}) {
+    const adRef = useRef(null)
     useEffect(()=>{
+        let isMounted = true;
+
         try{
-            (window.adsbygoogle =window.adsbygoogle || []).push({});
+            if(adRef.current && window.adsbygoogle && adRef.current.hasAttribute('data-adsbygoogle-status')){
+
+                (window.adsbygoogle =window.adsbygoogle || []).push({});
+            }
         }catch(error){
             console.log(error);
             
         }
-    })
+        return () =>{
+            isMounted = false;
+        }
+    },[])
   return (
     <div style={{ margin:'20px auto' }}>
       <ins 
-       className='adsbygoogle'
-       style={{ display:"block" }}
+      ref={adRef}
+       className='adsbygoogle'       s
+       style={{ display:"block" , minWidth:"300px",background:'#f8f9fa' }}
        data-ad-client = {adClient}
        data-add-slot={adSlot}
        data-ad-format="auto"
